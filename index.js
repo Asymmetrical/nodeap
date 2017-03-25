0e30fa2ca8555701c43dace0d59e13b2a7a307fa
Johan Wastring <johanwastring@Wastrinowerbook.lan>
2017-03-25 08:46:51 +0100

    startScript

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express')
 
 var app = express()
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res) {
     res.json({ notes: "This is index and start!" })
 })
@@ -10,4 +12,5 @@ app.get('/notes', function(req, res) {
     res.json({ notes: "This is your notebook. Edit this to start saving your notes!" })
 })
 
-app.listen(3000)
+//app.listen(3000)
+app.listen(process.env.PORT || 5000)
